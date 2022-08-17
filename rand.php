f258ec7e2d50c2eda4884734609a071107034c40
FCA1510 <fcoronel1510@gmail.com>
2022-08-16 22:35:49 -0300

    cambios

--- rand.php
+++ rand.php
@@ -4,7 +4,13 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-$valor = rand(1,5);
+$valor = rand(1, 5);
+
+if($valor == 1 || $valor == 3 || $valor == 5){
+    echo "El numero $valor es impar";
+}else {
+    echo "El numero $valor es par";
+};
 
 
 
