1a6fe6570d878b9c68b2a9b99b5397b98b176274
FCA1510 <fcoronel1510@gmail.com>
2022-08-18 19:55:36 -0300

    Cambios

--- rand.php
+++ rand.php
@@ -12,6 +12,17 @@ if($valor == 1 || $valor == 3 || $valor == 5){
     echo "El numero $valor es par";
 };
 
+// otra soluicion: echo $valor == 1 || &valor == 3 || $valor == 5 ? "El niumero $valor es impar" : "El numero $valor es par"
+
+/* Otra posible resolucion: 
+$valor = rand(1, 5);
+
+if($valor %2 == 0){
+    echo "El numero $valor es par";
+}else{
+    echo "El numero $valor es impar";
+}
+*/
 
 
 ?>
