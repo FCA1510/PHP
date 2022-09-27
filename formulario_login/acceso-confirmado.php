2f02821d3598e56f645a8b86730f6843077dddea
FCA1510 <fcoronel1510@gmail.com>
2022-09-27 18:22:11 -0300

    cambios

---
+++ formulario_login/acceso-confirmado.php
@@ -0,0 +1,40 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Acceso</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1>Bienvenid@ al sistema</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="alert alert-primary" role="alert">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus harum officiis libero quod, rerum adipisci reiciendis recusandae velit, laudantium tempore eos! Laboriosam suscipit repellat eligendi unde provident optio expedita debitis?
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <a href="index.php" class="btn btn-primary float-end">Salir</a>
+            </div>
+        </div> 
+    </div>
+</body>
+
+</html>
